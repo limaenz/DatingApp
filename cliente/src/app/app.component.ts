20922234c2592ef03cfe81b60491943c9bbcf3cb
Enzo Lima <enzolimafranca@outlook.com>
2023-08-14 11:19:33 -0300

    Criacao de estrutura e configuracao

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Dating app ';
   usuarios: any;
 
@@ -19,7 +19,4 @@ export class AppComponent implements OnInit{
       complete: () => console.log('Request realizado com sucesso.')
     })
   }
-
-
-
 }
